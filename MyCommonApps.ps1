ff48e7db04a5a5ccb44f61a6cb1e5073bc1f3b30
Joshua Betts <joshua.betts42@gmail.com>
2024-03-17 11:37:04 -0700

    Updated MyCommonApps.ps1 and README.md

--- MyCommonApps.ps1
+++ MyCommonApps.ps1
@@ -1,3 +1,34 @@
+<#
+.SYNOPSIS
+This script performs a fresh installation of Windows and configures various settings and applications.
+
+.DESCRIPTION
+This script automates the process of setting up a fresh installation of Windows. It performs the following steps:
+1. Disables UAC (User Account Control)
+2. Checks for Windows updates and installs them
+3. Uninstalls unnecessary applications that come with Windows out of the box
+4. Removes provisioned packages
+5. Configures Windows Subsystem for Linux and installs Ubuntu
+6. Installs applications specified in the 'apps.json' file
+7. Configures Windows Features
+8. Configures File Explorer Settings
+9. Configures Taskbar Settings
+10. Configures Windows Updates Settings
+11. Reboots the system
+
+.PARAMETER None
+This script does not accept any parameters.
+
+.EXAMPLE
+.\MyCommonApps.ps1
+Runs the script to perform the fresh installation and configuration of Windows.
+
+.NOTES
+- This script requires administrative privileges to run.
+- Make sure to review and modify the 'apps.json' file to specify the applications to be installed.
+- This script will automatically reboot the system after completing the configuration.
+#>
+
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 $logFile = Join-Path -Path $desktopPath -ChildPath 'AppDeployment.log'
 Start-Transcript -Path $logFile -Append
@@ -9,19 +40,15 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Disable UAC
-write-host "disabling UAC..."
+Write-Host "Disabling UAC..."
 Disable-UAC
 
-# ------------------------------
 # Step 1: Checking for Windows updates
-# ------------------------------
 Write-Host "Checking for Windows updates..."
 $updates = New-Object -ComObject Microsoft.Update.Session
 $updates.CreateUpdateSearcher().Search("IsInstalled=0 and Type='Software'").Updates
 
-# ------------------------------
 # Step 2: Downloading and installing updates
-# ------------------------------
 Write-Host "Downloading and installing updates..."
 foreach ($update in $updates) {
     Write-Host "Installing $($update.Title)..."
@@ -34,9 +61,43 @@ foreach ($update in $updates) {
     }
 }
 
-# ------------------------------
-# Step 3: Configuring Windows Subsystem for Linux and Ubuntu setup
-# ------------------------------
+# Step 3A: Uninstalling unnecessary applications that come with Windows out of the box
+Write-Host "Uninstalling unnecessary applications that come with Windows out of the box..."
+$applicationList = @(
+    "Microsoft.3DBuilder",
+    "Microsoft.BingFinance",
+    "Microsoft.BingNews",
+    "Microsoft.BingSports",
+    "Microsoft.BingWeather",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.Microsoft3DViewer",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.Print3D",
+    "Microsoft.SkypeApp",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+)
+foreach ($app in $applicationList) {
+    if (Get-WindowsPackage -Name $app -ErrorAction SilentlyContinue) {
+        Write-Host "Uninstalling $app..."
+        Uninstall-WindowsPackage -Name $app -Confirm:$false
+        Write-Host "$app has been uninstalled."
+    } else {
+        Write-Host "$app not found. Skipping..."
+    }
+}
+
+# Step 3B: Remove provisioned packages
+$packages = Get-AppxProvisionedPackage -Online
+foreach ($package in $packages) {
+    Remove-AppxProvisionedPackage -Online -PackageName $package.PackageName
+}
+
+# Step 4: Configuring Windows Subsystem for Linux and Ubuntu setup
 Write-Host "Configuring Windows Subsystem for Linux and Ubuntu setup..."
 
 Write-Host "Enabling Windows Subsystem for Linux..."
@@ -65,25 +126,19 @@ Write-Host "User Joshua created and sudo configured."
 
 Write-Host "Updating Ubuntu..."
 wsl -d Ubuntu -u joshua bash -c "sudo apt update && sudo apt upgrade -y"
-Write-host "Ubuntu has been updated."
+Write-Host "Ubuntu has been updated."
 
-# ------------------------------
-# Step 4: Installing applications
-# ------------------------------
+# Step 5: Installing applications
 Write-Host "Installing applications..."
 $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $jsonPath = Join-Path -Path $scriptPath -ChildPath 'apps.json'
 winget import -i $jsonPath
 
-# ------------------------------
-# Step 5: Configuring Windows Features
-# ------------------------------
+# Step 6: Configuring Windows Features
 Write-Host "Configuring Windows Features..."
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 
-# ------------------------------
-# Step 6: Configuring File Explorer Settings
-# ------------------------------
+# Step 7: Configuring File Explorer Settings
 Write-Host "Configuring File Explorer Settings..."
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
@@ -93,83 +148,25 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0
 
-# ------------------------------
-# Step 7: Configuring Taskbar Settings
-# ------------------------------
+# Step 8: Configuring Taskbar Settings
 Write-Host "Configuring Taskbar Settings..."
 Set-TaskbarOptions -Size Small -Dock Left -Combine Full -Lock
 Set-TaskbarOptions -Size Small -Dock Left -Combine Full -AlwaysShowIconsOn
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarAl -Value 0
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name PeopleBand -Value 0
 
-# ------------------------------
-# Step 8: Configuring Windows Updates Settings
-# ------------------------------
+# Step 9: Configuring Windows Updates Settings
 Write-Host "Configuring Windows Updates Settings..."
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name UxOption -Type DWord -Value 1
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config -Name DODownloadMode -Type DWord -Value 1
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\DeliveryOptimization -Name SystemSettingsDownloadMode -Type DWord -Value 3
 Write-Host "Windows Updates Settings configured."
 
-# ------------------------------
-# Step 9A: Uninstalling unecessary applications that come with Windows out of the box
-# ------------------------------
-Write-Host "Uninstalling unecessary applications that come with Windows out of the box..."
-$applicationList = @(
-    "Microsoft.BingFinance",
-    "Microsoft.3DBuilder",
-    "Microsoft.BingFinance",
-    "Microsoft.BingNews",
-    "Microsoft.BingSports",
-    "Microsoft.BingWeather",
-    "Microsoft.GetHelp",
-    "Microsoft.Getstarted",
-    "Microsoft.Messaging",
-    "Microsoft.Microsoft3DViewer",
-    "Microsoft.MicrosoftOfficeHub",
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.MicrosoftStickyNotes",
-    "Microsoft.Office.OneNote",
-    "Microsoft.OneConnect",
-    "Microsoft.People",
-    "Microsoft.Print3D",
-    "Microsoft.SkypeApp",
-    "Microsoft.StorePurchaseApp",
-    "Microsoft.Wallet",
-    "Microsoft.WindowsAlarms",
-    "microsoft.windowscommunicationsapps",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.WindowsMaps",
-    "Microsoft.WindowsSoundRecorder",
-    "Microsoft.YourPhone",
-    "Microsoft.ZuneMusic",
-    "Microsoft.ZuneVideo"
-)
-foreach ($app in $applicationList) {
-    if (Get-WindowsPackage -Name $app -ErrorAction SilentlyContinue) {
-        Write-Host "Uninstalling $app..."
-        Uninstall-WindowsPackage -Name $app -Confirm:$false
-        Write-Host "$app has been uninstalled."
-    } else {
-        Write-Host "$app not found. Skipping..."
-    }
-}
-
-# ------------------------------
-# Step 9B: Remove provisioned packages
-# ------------------------------
-$packages = Get-AppxProvisionedPackage -Online
-foreach ($package in $packages) {
-    Remove-AppxProvisionedPackage -Online -PackageName $package.PackageName
-}
-
-# Reenable UAC
-write-host "Enabling UAC..."
+# Re-enable UAC
+Write-Host "Enabling UAC..."
 Enable-UAC
 
-# ------------------------------
-# Step 9: Rebooting the system
-# ------------------------------
+# Step 10: Rebooting the system
 Write-Host "Rebooting the system..."
 shutdown.exe /r /t 30
 
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+/**
+ * @file README.md
+ * @brief WindowsFreshInstall
+ * 
+ * This file contains scripts for a fresh Windows deployment.
+ * 
+ * @details
+ * The WindowsFreshInstall project provides scripts that can be used for performing a fresh installation of Windows.
+ * These scripts are designed to automate the deployment process and make it easier to set up a new Windows system.
+ * 
+ * @note
+ * Before using these scripts, make sure to read the instructions and requirements provided in this README file.
+ * 
+ * @version 1.0
+ * @date 2022-01-01
+ */
 # WindowsFreshInstall
 For fresh windows installations
 These are scripts for a fresh windows deployment
